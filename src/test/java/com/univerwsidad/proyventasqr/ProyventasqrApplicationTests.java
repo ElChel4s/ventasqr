042672c14c520d8de62e9e74cd563d6a74c04b0e
Marcelo Alejandro Villarroel Gutierrez <marcelpro365@gmail.com>
2025-05-25 20:38:36 -0400

    Commit inicial del proyecto proyventasqr

---
+++ src/test/java/com/univerwsidad/proyventasqr/ProyventasqrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.univerwsidad.proyventasqr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyventasqrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
